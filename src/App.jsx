febfa59e6bd7ecc0307d56fe61f9bb498cd0222a
malish-stha <malish.shrestha@deerwalk.edu.np>
2024-11-28 13:56:35 +0545

    Add custom fonts and update styles in Tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Hello World</h1>
+      <h1 className="text-5xl text-orange-500 font-cicular-web">Hello World</h1>
     </main>
   );
 };
